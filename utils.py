2781ebe6332a2438aac126272f178e41f36d6d8f
b.ventura <b.ventura@reply.it>
2023-10-08 14:25:16 +0200

    working training

---
+++ utils.py
@@ -0,0 +1,54 @@
+from typing import Any, Tuple
+from functools import cache
+import config
+import time
+from collections.abc import Sequence, Mapping
+import httpx
+
+@cache
+def token() -> str:
+    with open("token.txt", "rt") as file:
+        return file.read()
+
+headers = {"Authorization": f"Bearer {token()}"}
+
+class JobException(Exception):
+    ...
+    
+def block_until_done(job_id: str) -> Mapping[str, Any]:
+    with httpx.Client() as client:
+        while True:
+            res = client.get(
+                f"{config.JOBCONTROL_API_JOBS_ENDPOINT}/{job_id}",
+                headers=headers
+            )
+            job = res.json()
+            status = job["status"]
+
+            if status == "Succeeded":
+                return job
+            elif status == "Pending" or status == "Running":
+                time.sleep(config.JOB_POLLING_INTERVAL_SECS)
+                # Continue polling...
+            else:
+                raise JobException(
+                    f"job {job_id} has unexpected status: {status}"
+                )
+
+def _artifacts(job: Mapping[str, Any]) -> Sequence[Tuple[str, str]]:
+    artifacts = job["outputs"]["artifacts"]
+
+
+    print(artifacts)
+    return tuple(
+        (item["name"], item["authorized_get_url"]) for item in artifacts
+    )
+
+def download_artifacts(job: Mapping[str, Any]) -> Mapping[str, Any]:
+
+    pairs = _artifacts(job)
+
+    with httpx.Client() as client:
+        return {
+            pair[0]: client.get(pair[1]).raise_for_status().read() for pair in pairs
+        }
