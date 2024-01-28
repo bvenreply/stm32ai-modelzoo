dd5b247e472d96b7fbf9ae6f8b00219b5b168528
b.ventura <b.ventura@reply.it>
2024-01-28 12:49:34 +0100

    fix(generic-notebook): more robust autoconfig

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
-import os
 
-VESPUCCI_BASE_URL = (
-    "http://jobcontrol-api:5002"
-    if os.getlogin() == "vespucciuser"
-    else "https://dev.stm-vespucci.com"
-)
+_default_base_url = "https://dev.stm-vespucci.com"
+
+try:
+    from getpass import getuser
+    VESPUCCI_BASE_URL = (
+        "http://jobcontrol-api:5002"
+        if getuser() == "vespucciuser"
+        else _default_base_url
+    )
+except Exception as _:
+    VESPUCCI_BASE_URL = _default_base_url
+
+
 JOBCONTROL_API_BASE_URL = f"{VESPUCCI_BASE_URL}/svc/jobcontrol/v1alpha1"
 JOBCONTROL_API_JOBS_ENDPOINT = f"{JOBCONTROL_API_BASE_URL}/jobs"
 TRAINING_JOB_TEMPLATE_ID = "01HM2EE60T3HJ4G5MPYK3GVT5D"
